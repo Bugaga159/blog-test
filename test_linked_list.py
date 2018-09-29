9e56a69915c45283beb492d55a608ed3df7de8f6
Andrew <santechserp@gmail.com>
2018-09-29 06:55:39 +0300

    tests Linked Lists

---
+++ test_linked_list.py
@@ -0,0 +1,105 @@
+import unittest
+import main
+
+
+class TestMain(unittest.TestCase):
+
+
+    # Тест на проверкудобавление в список
+    def test_return_all_nodes(self):
+        test1 = main.LinkedList()
+
+        # тест на проверку head = None
+        self.assertIsNone(test1.head)
+
+        test1.add_in_tail(main.Node(35))
+        ran = test1.return_all_nodes()
+
+        #тест на проверку данных в списке
+        self.assertEquals(ran, [35])
+
+        # тест на проверку head != None
+        self.assertIsNotNone(test1.head)
+
+
+    def test_remove(self):
+        test1 = main.LinkedList()
+        test1.add_in_tail(main.Node(35))
+        test1.add_in_tail(main.Node(54))
+        test1.add_in_tail(main.Node(35))
+        test1.add_in_tail(main.Node(64))
+        test1.remove(54)
+        test1.remove(64)
+        ran = test1.return_all_nodes()
+
+        # тест на проверку данных в списке
+        self.assertEquals(ran, [35, 35])
+
+    def test_removeAllVal(self):
+        test1 = main.LinkedList()
+        test1.add_in_tail(main.Node(35))
+        test1.add_in_tail(main.Node(54))
+        test1.add_in_tail(main.Node(35))
+        test1.add_in_tail(main.Node(64))
+        test1.removeAllVal(35)
+        test1.removeAllVal(64)
+        ran = test1.return_all_nodes()
+        self.assertEquals(ran, [54])
+
+    def test_clearAll(self):
+        test1 = main.LinkedList()
+        test1.add_in_tail(main.Node(35))
+        test1.add_in_tail(main.Node(54))
+        res = test1.clearAll()
+        self.assertEquals(res, None)
+        self.assertIsNone(res)
+
+    def test_findVal(self):
+        test1 = main.LinkedList()
+        test1.add_in_tail(main.Node(35))
+        test1.add_in_tail(main.Node(54))
+        test1.add_in_tail(main.Node(35))
+        test1.add_in_tail(main.Node(12))
+        res1 = test1.findVal(35)
+        res2 = test1.findVal(14)
+        self.assertEquals(res1, [[35, 1], [35, 3]])
+        self.assertEquals(res2, [])
+
+
+    def test_lengthList(self):
+        test1 = main.LinkedList()
+        test1.add_in_tail(main.Node(35))
+        test1.add_in_tail(main.Node(54))
+        test1.add_in_tail(main.Node(35))
+        test1.add_in_tail(main.Node(64))
+        res = test1.lengthList()
+        self.assertEquals(res, 4)
+        self.assertIsNotNone(res)
+
+    def test_add_val_after(self):
+        test1 = main.LinkedList()
+        test1.add_in_tail(main.Node(35))
+        test1.add_in_tail(main.Node(54))
+        test1.add_in_tail(main.Node(35))
+        test1.add_in_tail(main.Node(64))
+        test1.add_val_after(35, 75)
+        res1 = test1.return_all_nodes()
+        self.assertEquals(res1, [35, 75, 54, 35, 64])
+        test1.add_val_after(64, 75)
+        res2 = test1.return_all_nodes()
+        self.assertEquals(res2, [35, 75, 54, 35, 64, 75])
+
+    def test_Comparison(self):
+        test1 = main.LinkedList()
+        test2 = main.LinkedList()
+        test1.add_in_tail(main.Node(35))
+        test1.add_in_tail(main.Node(54))
+        test2.add_in_tail(main.Node(35))
+        test2.add_in_tail(main.Node(64))
+
+        res = main.Comparison(test1, test2)
+        self.assertEquals(res, [70, 118])
+
+
+if __name__ == "__main__":
+    unittest.main()
