3fff7b976d3ccd7f9181e69f455d98c511da4e60
Andrew <santechserp@gmail.com>
2018-10-20 13:55:26 +0300

    Тесты для проверки модуля Stack

--- module/Stack/test_stack.py
+++ module/Stack/test_stack.py
@@ -10,6 +10,58 @@ class TestStack(unittest.TestCase):
         test1.push(52)
         test1.push(8)
 
+        #   Проверка метода, удаляет и выводит первый
+        #   элемент стека( 35 == 35)
         self.assertEqual(test1.pop_head(), 35)
 
+        #   Проверка метода, удаляет и выводит первый
+        #   элемент стека( 52 == 52)
         self.assertEqual(test1.pop_head(), 52)
+        test1.pop()
+
+        #   Проверка стека на None, стек пустой
+        self.assertEqual(test1.pop_head(), None)
+        self.assertIsNone(test1.pop_head())
+
+    def tets_peak_head(self):
+
+        test2 = Stack.Stack()
+        test2.push(75)
+        test2.push(56)
+        test2.push(12)
+        test2.push(35)
+
+        #   Проверка метода, выводит первый
+        #   элемент стека( 75 == 75)
+        self.assertEqual(test2.peak_head(), 75)
+
+        #  Удаляем первый элемент стека
+        test2.pop_head()
+
+        #   Проверка метода, выводит первый
+        #   элемент стека( 56 == 56)
+        self.assertEqual(test2.peak_head(), 56)
+
+        #   Удаляем все элементы стека
+        while test2.size() > 0:
+            test2.pop()
+
+        #   Проверка стека на None, стек пустой
+        self.assertEqual(test2.peak_head(), None)
+        self.assertIsNone(test2.peak_head())
+
+    def test_balance_brackets(self):
+        # данные для проверки корректные
+        test1 = Stack.balance_brackets('()()')
+
+        # данные для проверки не корректные
+        test2 = Stack.balance_brackets(')()(')
+        # данные для проверки корректные
+        test3 = Stack.balance_brackets('(()(()()))')
+        # данные для проверки не корректные, кол-во скобок разное
+        test4 = Stack.balance_brackets('(()))')
+
+        self.assertTrue(test1)
+        self.assertFalse(test2)
+        self.assertTrue(test3)
+        self.assertFalse(test4)
