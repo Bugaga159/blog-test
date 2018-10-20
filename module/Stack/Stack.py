ee89afa79436c4857bee4fa30518bc205991ac82
Andrew <santechserp@gmail.com>
2018-10-20 03:52:39 +0300

    добавил модуль Stack

--- module/Stack/Stack.py
+++ module/Stack/Stack.py
@@ -29,3 +29,34 @@ class Stack:
       if len(self.stack) == 0:
             return None
       return self.stack[0]
+
+
+def balance_brackets(x):
+    checkArr = Stack()
+    x1 = x2 = 0
+    if (len(x) % 2) != 0:       #Проверка на четное количество элементов
+        return False
+    for i in range(len(x)):     #Для проверки на равное количество скобок откр/закр
+        if x[i] == '(':
+            x1 +=1
+        elif x[i] == ')':
+            x2 +=1
+    if x1 == x2:
+        for i in range(len(x)):
+            if checkArr.size() == 0 and x[i] == '(':
+                checkArr.push(x[i])
+            elif checkArr.peak_head() != x[i]:
+                checkArr.pop()
+            else:
+                checkArr.push(x[i])
+
+    else:
+        return False
+    if checkArr.size() == 0:
+        return True
+    else:
+        return False
+
+print(balance_brackets('(()((())()))'))
+
+
