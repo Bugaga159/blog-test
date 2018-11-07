531f12fb6d29672f4bc40c3c974157f994ff7a3a
Andrew <santechserp@gmail.com>
2018-11-08 01:28:19 +0300

    Проверка на полиндром строки

---
+++ module/Deque/Deque.py
@@ -0,0 +1,29 @@
+class Deque:
+    def __init__(self):
+        self.queue = []
+
+    def addFront(self, item):
+        self.queue.append(item)
+
+    def addTail(self, item):
+        self.queue.insert(0,item)
+
+    def removeFront(self):
+        return self.queue.pop()
+
+    def removeTail(self):
+        return self.queue.pop(0)
+
+    def size(self):
+        return len(self.queue)
+
+# Проверяет строку, является ли некоторая строка палиндромом
+def palindrome(strCheck):
+    n = Deque()
+    n.queue = list(strCheck)
+    while n.size() != 0:
+        if n.removeFront() == n.removeTail():
+            continue
+        else:
+            return False
+    return True
