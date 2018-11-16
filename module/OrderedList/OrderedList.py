116ce14477123af1f023009a49b4653c503940f1
Andrew <santechserp@gmail.com>
2018-11-16 06:51:12 +0300

    Update OrderedList.py

--- module/OrderedList/OrderedList.py
+++ module/OrderedList/OrderedList.py
@@ -1,68 +1,66 @@
-class Node2:
+class Node:
     def __init__(self, v):
         self.value = v
         self.next = None
 
 class OrderedList:
   def __init__(self,s = 0):
-    self.head = None
-    self.tail = None
-    self.sort = None
+        self.head = None
+        self.tail = None
+        self.sort = None
 
-    if s == 0:
-      self.sort = 'ASC'
-    elif s == 1:
-      self.sort = 'DESC'
-  # Метод сравнения двух значений
+        if s == 0:
+          self.sort = 'ASC'
+        elif s == 1:
+          self.sort = 'DESC'
+
+    # Метод сравнения двух значений
   def comparison(self, val1, val2):
-    if val1 > val2:
-      return True
-    elif val1 <= val2:
-      return False
+        if int(val1) > int(val2):
+          return True
+        else:
+          return False
         
   # Добавление в список
-  def add_in_tail(self, item):
-    if self.head is None:
+  def add_in_tail(self, item):    
+    if self.head  is None:
       self.head = self.tail = item
     else:
       node = self.head
-      while node is not None:
-        if self.comparison(node.value, item.value):
-          prev = item
-          prev.next = self.head
-          self.head = prev
-          break
-        elif node.next is None:
-          node.next = item
-          self.tail = item
-          break
-        node = node.next
+      if self.comparison(node.value, item.value):
+        prev = item
+        prev.next = node
+        self.head = prev
+      elif node.next is None:
+        node.next = item
+        self.tail = item     
+      else:
+        while node.next is not None:
+          if self.comparison(node.next.value, item.value):
+            prev = item
+            prev.next = node.next
+            node.next = prev
+            break
+          else:
+            node = node.next
+            if node.next is None:
+              node.next = item
+              self.tail = item
+              break
 
-
-  # Удалить заданное значение в связанном списке все
-  def removeAllVal(self, val):
-    if self.head.value == val:
-      self.head = self.head.next
-    if self.head == None:
-      return None
-    else:
-      node = self.head
-      while node.next is not None:
-        if node.next.value == val:
-          if self.tail == node.next:
-            self.tail == node.next.next
-          node.next = node.next.next
-        else:
-          node = node.next
-
-
-
-    
-test = OrderedList()
-test.add_in_tail(Node2(2))
-test.add_in_tail(Node2(1))
-test.add_in_tail(Node2(3))
-
-print(test.head.next.value)
-
-
+  # Удалить значение в связанном списке
+  def remove(self, val):
+      if self.head == None:
+          return None
+      elif self.head.value == val:
+          self.head = self.head.next
+      else:
+          node = self.head
+          while node.next is not None:
+              if node.next.value == val:
+                  if self.tail == node.next:
+                      self.tail == node.next.next
+                  node.next = node.next.next
+                  break
+              node = node.next
+ 
