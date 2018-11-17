e2f71e8c6d00169fc8a184ecd0b98fd8a7109b47
Andrew <santechserp@gmail.com>
2018-11-17 15:36:45 +0300

    сравнение строк работает

--- module/OrderedList/СomparisonStrings.py
+++ module/OrderedList/СomparisonStrings.py
@@ -1,11 +1,24 @@
-from .OrderedList import *
+import OrderedList as OL
 
 
-class СomparisonStrings(OrderedList):
-
+class СomparisonStrings(OL.OrderedList):
+    def add_in_tail(self, item):
+        item.value = item.value.strip()
+        super().add_in_tail(item)
+        print('ok')
 
     def comparison(self, val1, val2):
-        if int(val1) > int(val2):
+        if val1 > val2:
           return True
         else:
           return False
+
+test = СomparisonStrings()
+test.add_in_tail(OL.Node('hello'))
+test.add_in_tail(OL.Node('  who are you'))
+test.add_in_tail(OL.Node(' bay!  '))
+
+print(test.head.value)
+print(test.head.next.value)
+print(test.head.next.next.value)
+
