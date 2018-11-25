01c1c5bd44ec18411b5269b591e8f587f4bcb110
Andrew <santechserp@gmail.com>
2018-11-25 17:56:01 +0300

    Написан код для HashTable и тесты

--- module/HashTable/HashTable.py
+++ module/HashTable/HashTable.py
@@ -6,27 +6,41 @@ class HashTable:
         
     # Вычисляет индекс слота
     def hash_fun(self, value):
-      key = 0
-      byteArr =  list(map(ord, str(value)))
-      for i in range(len(byteArr)):
-        key += byteArr[i]
-      key = key % self.size
-      return key
+        key = 0
+        byteArr = list(map(ord, str(value)))
+        for i in range(len(byteArr)):
+            key += byteArr[i]
+        key = key % self.size
+        return key
       
     # метод  поиска слота, значению сперва рассчитывает 
     # индекс хэш-функцией, а затем отыскивает подходящий 
     # слот для него с учётом коллизий, или возвращает None
     def seek_slot(self, value):
-      index = self.hash_fun(value)
-      print(index)
-      
-      for i in range(self.size):
-        if self.slots[index] is None:
-          self.slots[index] = value
-          return True
+        index = self.hash_fun(value)
+        for i in range(self.size):
+            if index > self.size-1:
+                return None
+            if self.slots[index] is None:
+                return index
+            else:
+                if index == i:
+                    index += self.step
+        return None
+
+    # помещает значение value в слот,
+    # вычисляемый с помощью функции поиска
+    def put(self, value):
+        index = self.seek_slot(value)
+        if index != None:
+            self.slots.insert(index, value)
         else:
-          index += self.step
-          i += self.step
-        if i > self.size:
-          return None
-      return None
+            return None
+
+    def find(self, value):
+        for i in range(self.size):
+            if self.slots[i] == value:
+                return i
+        return None
+
+
--- module/HashTable/test.py
+++ module/HashTable/test.py
@@ -1,1 +1,53 @@
+import unittest
+from . import HashTable
 
+class TestHashTable(unittest.TestCase):
+
+    def test_hash_fun(self):
+        test = HashTable.HashTable(17, 3)
+        a = test.hash_fun(0)
+
+        # символ - 0, в Юникоде в десятичной системе исчисления равен 48
+        b = 48 % test.size
+
+        self.assertEqual(b, a)
+
+        c = test.hash_fun('a')
+        # символ - a, в Юникоде в десятичной системе исчисления равен 48
+        d = 97 % test.size
+        self.assertEqual(d, c)
+
+    def test_seek_slot(self):
+        test = HashTable.HashTable(17, 3)
+        a = test.seek_slot(0)
+        # символ - 0, в Юникоде в десятичной системе исчисления равен 48
+        b = 48 % test.size
+        self.assertEqual(b, a)
+
+        c = test.seek_slot('a')
+        # символ - a, в Юникоде в десятичной системе исчисления равен 48
+        d = 97 % test.size
+        self.assertEqual(d, c)
+
+    def test_put(self):
+        test = HashTable.HashTable(17, 3)
+        test.put(0)
+        # символ - 0, в Юникоде в десятичной системе исчисления равен 48
+        a = 48 % test.size
+        self.assertEqual(0, test.slots[a])
+
+        self.assertIsNone(test.put(0))
+
+        test.put('a')
+        b = 97 % test.size
+        self.assertEqual('a', test.slots[b])
+
+    def test_find(self):
+        test = HashTable.HashTable(17, 3)
+        test.put(0)
+        a = 48 % test.size
+        self.assertEqual(a, test.find(0))
+        test.put('a')
+        b = 97 % test.size
+        self.assertEqual(b, test.find('a'))
+        
