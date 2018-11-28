62ffb0892b6e7fbdcca49c20afc536f721985d5a
Andrew <santechserp@gmail.com>
2018-11-28 05:26:44 +0300

    наследует функции HashTable

--- module/NativeDictionary/NativeDictionary.py
+++ module/NativeDictionary/NativeDictionary.py
@@ -1,56 +1,32 @@
+from . import HashTable
 
-class NativeDictionary:
+class NativeDictionary(HashTable):
     def __init__(self, sz, stp):
         self.size = sz
         self.step = stp
         self.slots = [None] * self.size
-        self.dictionary = {}
-        
-    # Вычисляет индекс слота
-    def hash_fun(self, value):
-        return sum(list(map(ord, str(value)))) % self.size
-      
-    # метод  поиска слота, значению сперва рассчитывает 
-    # индекс хэш-функцией, а затем отыскивает подходящий 
-    # слот для него с учётом коллизий, или возвращает None
-    def seek_slot(self, value):
-        index = self.hash_fun(value)
-        for i in range(self.size):
-            if index > self.size-1:
-                return None
-            if self.slots[index] is None:
-                return index
-            else:
-                if index == i:
-                    index += self.step
-        return None
+        self.dictionary = [None] * self.size
 
-    # помещает значение value в слот,
-    # вычисляемый с помощью функции поиска
-    def put(self, key, value): 
-        if self.is_key(key) is not None:
+
+    def put(self, key, value):
+        if self.find(key) is not None:
           self.dictionary[key] = value
         else:
-            index = self.seek_slot(key)
-            if index != None:
-                self.slots[index] = key
-                self.dictionary[key] = value
-            else:
-                return None
-    # проверяет, имеется ли в слотах указанное значение, 
-    # и возвращает либо слот, либо None.
+          index = self.seek_slot(key)
+          if index is not None:
+              self.slots[index] = key
+              self.dictionary[index] = value
+          else:
+              return None
+
     def is_key(self, key):
-        index = self.hash_fun(key)
-        if self.slots[index] == key:
+        index = self.find(key)
+        if index is not None:
             return True
         else:
-            for i in range(self.size):
-                if self.slots[i] == key:
-                    return True
-        return None
-    def get(self, key):
-        index = self.is_key(key)
-        if index:
-            return self.dictionary[key]
-        else:
-          return None
+            return None
+
+
+    def get(self, key): 
+        return self.dictionary[self.find(key)]
+  
